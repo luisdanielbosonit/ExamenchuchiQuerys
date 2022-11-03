b03df1e8481ece0de2c0c107147b60e65d13be5b
luisdaniel.acosta <luisdaniel.acosta@bosonit.com>
2022-11-03 21:06:46 +0100

    postman

--- src/main/java/com/bosonit/training/chuchi2/Controller.java
+++ src/main/java/com/bosonit/training/chuchi2/Controller.java
@@ -3,6 +3,7 @@ package com.bosonit.training.chuchi2;
 import com.sun.istack.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.HashMap;
 
@@ -13,16 +14,24 @@ public class Controller {
     ServiceImpl service;
 
     @GetMapping(value ={"*","/{path1}","/{path1}/{path2}"})
-    public Objetc12 entrOther(@PathVariable(required = false)@NotNull String path1,
-                              @PathVariable(required = false) String path2,
-                              @RequestParam(required = false) HashMap<String,String> query,
-                              @RequestHeader(required = true) HashMap<String,String> header){
+    public Object entrOther(@PathVariable(required = false)@NotNull String path1,
+                            @PathVariable(required = false) String path2,
+                            @RequestParam(required = false) HashMap<String,String> query,
+                            @RequestHeader(required = false) HashMap<String,String> header){
+        if(!(!header.containsKey("redirige") || header.values().equals("SALTA"))) {
+            return new ModelAndView("redirect:/salt");
+        }
+
         return new Objetc12(path1, path2,query,header);
     }
 
     @GetMapping({"/","one"})
     public String entrOne(){
         return "tienes One";
+    }
+    @GetMapping(value={"/salt"})
+    public String entryJump() {
+        return "he ido a Jump";
     }
 }
 
